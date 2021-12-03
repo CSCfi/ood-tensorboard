1851eb352c038bb863ea51ec9630e77da693042c
Robin Karlsson <robin.karlsson@csc.fi>
2021-12-03 12:48:41 +0200

    Add first working version

---
+++ template/after.sh
@@ -0,0 +1,13 @@
+# wait for the Tensorboard server to start
+echo "$(date): waiting for Tensorboard server to open port ${app_port}..."
+
+if wait_until_port_used "${host}:${port}" 300; then
+    echo "$(date): discovered Tensorboard server listening on port ${app_port}!"
+else
+  echo "$(date): timed out waiting for Tensorboard server to open port ${app_port}!"
+  echo "- wait ended at: $(date)"
+  pkill -P ${SCRIPT_PID}
+  clean_up 1
+fi
+
+sleep 2
